c7cdf4eeb791bbe77ef4f8335f933a17dde674a1
S B ARJUN DHILIP <arjundhilip38@gmail.com>
2024-02-15 16:24:06 +0530

    first commt

--- components/Districts/Coimbatore.tsx
+++ components/Districts/Coimbatore.tsx
@@ -1,11 +1,114 @@
 import React from 'react'
+import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
+import Image from 'next/image'
+
+const features = [
+  {
+    name: 'Push to deploy.',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: CloudArrowUpIcon,
+  },
+]
+
+const products = [
+  {
+      id: 1,
+      name: 'Thirunelvi Halwa',
+      href: '#',
+      price: '$48',
+      imageSrc: '/food1.jpg',
+      imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
+  },
+  {
+      id: 2,
+      name: 'Manaparai Murukku',
+      href: '#',
+      price: '$35',
+      imageSrc: '/food3.jpg',
+      imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
+  },
+  {
+      id: 3,
+      name: 'Kovilpatti Kadalamittai',
+      href: '#',
+      price: '$89',
+      imageSrc: '/food1.jpg',
+      imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+  },
+  {
+      id: 4,
+      name: 'Butter Chicken',
+      href: '#',
+      price: '$35',
+      imageSrc: '/food3.jpg',
+      imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+  },
+]
+
 
 const Coimbatore = () => {
   return (
-    <div  className= "mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-      Welcome to Coimbatore Page
+    <section>
+    <div className="overflow-hidden bg-white sm:py-20">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+          <div className="lg:pr-8 lg:pt-4">
+            <div className="lg:max-w-lg">
+              <h2 className="text-base font-semibold leading-7 text-indigo-600">#D1</h2>
+              <p className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl">Coimbatore</p>
+              <p className="mt-6 text-lg leading-8 text-gray-600">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, 
+              when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+              </p>
+              <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+                {features.map((feature) => (
+                  <div key={feature.name} className="relative pl-9">
+                    <dt className="inline font-semibold text-gray-900">
+                      <feature.icon className="absolute left-1 top-1 h-5 w-5 text-indigo-600" aria-hidden="true" />
+                      {feature.name}
+                    </dt>{' '}
+                    <dd className="inline">{feature.description}</dd>
+                  </div>
+                ))}
+              </dl>
+            </div>
+          </div>
+          <Image
+            src="https://www.google.com/maps/vt/data=Rh5eSgMARZidU-wt837Lc4mrhgcmPEioEXfLm1Mz1xFF6QpJfVZljf7UHEuBtQ-1vqXAv2-Y3OIEzN60t46FNVzMRHXwK7FrJZOjAJmcWJiojfPW4Ostn0QFq6YHAPhXGJllm74slwx4NvJ3rDLwf4ei8-0xII_9m_bVEmwHVaI_u7XYNkxrWGjwNRDmqKkhORJJgXVDNPS2Qm9nw5fITYHiFd6ak5WOHNDOHzpkoBvy017g1AVxjX3TF2Nnwzfb-c7sOwmyrMk4TOGBxb0tnSM4DJE4CHkS6OVc"
+            alt="Product screenshot"
+            className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+            width={2432}
+            height={1442}
+          />
+        </div>
+      </div>
     </div>
+    <div className="bg-white">
+    <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <h1 className="mt-4 text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Signature Dishes</h1>
+        <div className="mt-10 flex items-center justify-center grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+            {products.map((product) => (
+                <a key={product.id} href={product.href} className="group">
+                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                        <Image
+                            src={product.imageSrc}
+                            alt={product.imageAlt}
+                            width={400}
+                            height={400}
+                            className="h-full w-full object-cover object-center group-hover:opacity-75"
+                        />
+                    </div>
+                    <h3 className="bold-20 lg:bold-20 mt-4 text-center text-sm text-gray-700">{product.name}</h3>
+                </a>
+            ))}
+        </div>
+    </div>
+    </div>
+    </section>
   )
 }
+
 
 export default Coimbatore
--- components/Map.tsx
+++ components/Map.tsx
@@ -1,40 +1,33 @@
 "use client"
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup,Polygon,Polyline,PolygonProps } from 'react-leaflet'
 import "leaflet/dist/leaflet.css"
 import "leaflet-defaulticon-compatibility"
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
 import Link from 'next/link'
 
+import {statesData} from '../resources/data'
+
 
 const Map = () => {
   return (
-    <div className="container mx-auto content-center">
-      <MapContainer center={[11.0205, 76.9666]} zoom={14} scrollWheelZoom={false} style={{ height: "500px", width: "90%", }}>
+    <div className="container m-0.5 place-content-center">
+      <MapContainer center={[11.127123, 78.656891]} zoom={7} scrollWheelZoom={true} style={{ height: "500px", width: "vw", alignItems : "center"}}>
         <Marker
           position={[11.0205, 76.9666]}
           draggable={true}
         >
           <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution= '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+            url="https://api.maptiler.com/maps/openstreetmap/256/{z}/{x}/{y}.jpg?key=7i4sP5BRf51l7b2tlZL8"
           />
           <Popup className='font-bold text-lg leading-8 text-gray-600'>
             <Link href="/districts/coimbatore"> Coimbatore</Link>
           </Popup>
         </Marker>
+        
+        
 
-        <Marker
-          position={[13.0827, 80.2707]}
-          draggable={true}
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Popup className='font-bold text-lg leading-8 text-gray-600'>
-            <Link href="/districts/chennai"> Chennai</Link>
-          </Popup>
-        </Marker>
+        
 
       </MapContainer>
     </div>
