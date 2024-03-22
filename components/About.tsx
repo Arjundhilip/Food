fa9a1c0303e44d71830a96bc06e02639780b4523
S B ARJUN DHILIP <arjundhilip38@gmail.com>
2024-03-22 13:57:56 +0530

    first commi

--- components/About.tsx
+++ components/About.tsx
@@ -45,7 +45,7 @@ const About = () => {
   return (
     <div className="overflow-hidden bg-white py-24 sm:py-12">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-4 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-8 lg:pt-4">
             <div className="lg:max-w-lg">
               <h2 className="text-base font-semibold leading-7 text-indigo-600">About Us</h2>
@@ -69,7 +69,7 @@ const About = () => {
           <Image
             src="/districts/Chennai/Chennai_1.jpg"
             alt="Product screenshot"
-            className=" aspect-[4/5] w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+            className=" aspect-[9/14] border-2 rounded-xl mt-20 md:-ml-2 lg:-ml-0"
             width={2432}
             height={1442}
           />
