0a8a375b9c21c978b0faaab6c11cf39b79854c40
S B ARJUN DHILIP <arjundhilip38@gmail.com>
2024-03-25 16:03:58 +0530

    fir

--- constants/districts/chennai.ts
+++ constants/districts/chennai.ts
@@ -130,7 +130,7 @@ export const popular = [
         id: 1,
         name: 'Chicken-65',
         href: '/dishes/popular/chicken65',
-        imageSrc: '/dishes/vadacurry/top.jpg',
+        imageSrc: '/dishes/chicken65/top.jpg',
         imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
     },
   
